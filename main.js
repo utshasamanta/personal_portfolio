0fc988c2d0d56fd6ebc929f3d2e9a0b627bd474d
Utsha Samanta <utshalm@gmail.com>
2025-05-07 22:52:47 -0600

    added header and icons

---
+++ main.js
@@ -0,0 +1,44 @@
+//For text overlay
+let headerText = document.querySelector(".header-text");
+let overlayText = document.querySelector(".header-text[data-overlay]");
+
+overlayText.innerText = headerText.textContent;
+
+
+
+
+
+
+//For Canvas
+var c = document.getElementById('canv');
+var $ = c.getContext('2d');
+
+var col = function(x, y, r, g, b) {
+  $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+  $.fillRect(x, y, 1,1);
+}
+var R = function(x, y, t) {
+  return( Math.floor(192 + 64*Math.cos( (x*x-y*y)/300 + t )) );
+}
+
+var G = function(x, y, t) {
+  return( Math.floor(192 + 64*Math.sin( (x*x*Math.cos(t/4)+y*y*Math.sin(t/3))/300 ) ) );
+}
+
+var B = function(x, y, t) {
+  return( Math.floor(192 + 64*Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
+}
+
+var t = 0;
+
+var run = function() {
+  for(let x=0;x<=35;x++) {
+    for(let y=0;y<=35;y++) {
+      col(x, y, R(x,y,t), G(x,y,t), B(x,y,t));
+    }
+  }
+  t = t + 0.01;
+  window.requestAnimationFrame(run);
+}
+
+run();
